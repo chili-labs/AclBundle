4eaf905f548d6a9de9beaad8493700fccfdf884d
Daniel Tschinder <daniel.tschinder@researchgate.net>
2014-06-06 14:41:34 +0200

    Warmup cache before running tests

---
+++ Tests/bootstrap.php
@@ -0,0 +1,22 @@
+<?php
+
+use Symfony\Bundle\FrameworkBundle\Command\CacheWarmupCommand;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Output\NullOutput;
+use Symfony\Component\Console\Input\ArrayInput;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/App/AppKernel.php';
+
+$kernel = new AppKernel('test', true);
+$kernel->boot();
+
+$application = new Application($kernel);
+
+$command = new CacheWarmupCommand();
+$application->add($command);
+$input = new ArrayInput(array(
+    'command' => 'cache:warmup',
+    '--env'   => 'test'
+));
+$command->run($input, new NullOutput());
